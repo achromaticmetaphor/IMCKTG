2b4f11b9b6130a4e4b2ac1d79da4c6711209cfba
Charles Cagle <achromaticmetaphor@gmail.com>
2014-08-14 20:29:26 -0400

    add About screen

--- src/us/achromaticmetaphor/imcktg/GaAT.java
+++ src/us/achromaticmetaphor/imcktg/GaAT.java
@@ -3,9 +3,13 @@ package us.achromaticmetaphor.imcktg;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class GaAT extends Activity {
+
+  private final String menuAbout = "About";
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +27,21 @@ public class GaAT extends Activity {
 
   public void inputToFile(View view) {
     startActivity(new Intent(this, ChooseFilename.class));
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    super.onCreateOptionsMenu(menu);
+    menu.add(menuAbout);
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem mi) {
+    super.onOptionsItemSelected(mi);
+    if (mi.getTitle().equals(menuAbout))
+      startActivity(new Intent(this, About.class));
+    return true;
   }
 
 }
