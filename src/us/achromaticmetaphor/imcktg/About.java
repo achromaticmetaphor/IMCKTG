a9841337c669854ea9f402521f709f2cce75b28e
Charles Cagle <achromaticmetaphor@gmail.com>
2015-11-02 03:35:51 -0500

    separate about text

--- src/us/achromaticmetaphor/imcktg/About.java
+++ src/us/achromaticmetaphor/imcktg/About.java
@@ -1,17 +1,32 @@
 package us.achromaticmetaphor.imcktg;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.text.method.ScrollingMovementMethod;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class About extends Activity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_about);
-    ((TextView) findViewById(R.id.About)).setMovementMethod(new ScrollingMovementMethod());
+    setContentView(R.layout.activity_listview);
+    final String [] cmds = new String [] {"About app", "About icons"};
+    final ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, cmds);
+    ListView listView = (ListView) findViewById(R.id.cmdlist);
+    listView.setAdapter(adapter);
+    listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+      public void onItemClick(AdapterView<?> av, View v, int pos, long id) {
+        Intent intent = new Intent(About.this, AboutText.class);
+        intent.putExtra("about_text", pos == 0 ? R.string.about_code : R.string.about_icons);
+        startActivity(intent);
+      }
+    });
   }
 
 }
---
+++ src/us/achromaticmetaphor/imcktg/AboutText.java
@@ -0,0 +1,19 @@
+package us.achromaticmetaphor.imcktg;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.TextView;
+
+public class AboutText extends Activity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_about);
+    TextView tv = (TextView) findViewById(R.id.about);
+    tv.setMovementMethod(new ScrollingMovementMethod());
+    tv.setText(getIntent().getIntExtra("about_text", 0));
+  }
+
+}
